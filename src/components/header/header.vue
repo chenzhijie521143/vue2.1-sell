c40c3c951545960a45ad3c7d1557121885f3eb86
chenzhijie <chenzhijie521143@163.com>
2018-03-12 19:38:07 +0800

    add detail and star

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="header">
+  	<!--内容区-->
   	<div class="content-wrapper">
   	  <div class="avatar">
   	    <img v-bind:src="seller.avatar" width="64" height='64'/>
@@ -10,33 +11,80 @@
   	  		<span class="name">{{seller.name}}</span>
   	  	</div>
   	  	<div class="description">
-  	  		 {{seller.description}}/{{seller.deliveryTime}}分钟送达
+  	  		{{seller.description}}/{{seller.deliveryTime}}分钟送达
   	  	</div>
   	  	<div v-if="seller.supports" class="support"><!--判断是否存在-->
   	  		<span class="icon" v-bind:class="classMap[seller.supports[0].type]"></span>
   	  		<span class="text">{{seller.supports[0].description}}</span>
   	  	</div>
   	  </div>
-  	  <div v-if="seller.supports" class="support-count">
-  	  	 <span class="count">{{seller.supports.length}}个</span>
-  	  	 <i class="icon-keyboard_arrow_right"></i>
+  	  <div v-if="seller.supports" class="support-count" @click="showDetail">
+  	  	<span class="count">{{seller.supports.length}}个</span>
+  	  	<i class="icon-keyboard_arrow_right"></i>
   	  </div>
   	</div>
-  	<div class="bulletin-wrapper">
+  	<!--公告区-->
+  	<div class="bulletin-wrapper" @click="showDetail">
   		<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
   		<i class="icon-keyboard_arrow_right"></i>
   	</div>
+  	<!--背景-->
   	<div class="background">
   		<img v-bind:src="seller.avatar" width="100%" height="100%" />
   	</div>
+  	<!--遮罩层 ，详情页-->
+  	<transition name="fade">
+	  	<div class="detail" v-show="detailShow">
+	  		<div class="detail-wrapper clearfix">
+	  			<div class="detail-main">
+	  				<h1 class="name">{{seller.name}}</h1>
+	  				<div class="star-wrapper">
+	  					<star :size="48" :score="seller.score"></star>
+	  				</div>
+	  				<div class="title">
+	  					<div class="line"></div>
+	  					<div class="text">优惠信息</div>
+	  					<div class="line"></div>
+	  				</div>
+	  				<ul v-if="seller.supports" class="supports">
+	  					<li class="support-item" v-for="(item ,index) in seller.supports">
+	  					  <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+	  					  <span class="text">{{seller.supports[index].description}}</span>
+	  					</li>
+	  				</ul>
+	  				<div class="title">
+	  					<div class="line"></div>
+	  					<div class="text">商家公告</div>
+	  					<div class="line"></div>
+	  				</div>
+	  				<div class="bulletin">
+	  					 <p class="content">{{seller.bulletin}}</p>
+	  				</div>
+	  			</div>
+	  		</div>
+	  		<div class="detail-close" @click="hideDetail">
+	  			<i class="icon-close"></i>
+	  		</div>
+	  	</div>
+	  </transition>
   </div>
 </template>
 
 <script>
+import star from "../star/star.vue";
 export default{
 	data(){
 		return{
-			classMap:[]
+			classMap:[],
+			detailShow:false
+		}
+	},
+	methods:{
+		showDetail(){
+			this.detailShow=true;
+		},
+		hideDetail(){
+			this.detailShow=false;
 		}
 	},
   props: {
@@ -47,6 +95,9 @@ export default{
   },
   created (){
   	this.classMap=['decrease','discount','guarantee','invoice','special'];
+  },
+  components:{
+  	star
   }
 };
 </script>
@@ -56,6 +107,7 @@ export default{
   
 .header {
 	position:relative;
+	overflow:hidden;
 	color:#fff;
 	background:rgba(7,17,27,0.5);
 	.content-wrapper {
@@ -99,7 +151,7 @@ export default{
 			.support {
 				.icon {
 					display:inline-block;
-					vertical-align: top;
+					vertical-align:top;
 					width:12px;
 					height:12px;
 					margin-right: 4px;
@@ -159,7 +211,7 @@ export default{
 		.bulletin-title{
 			display:inline-block;
 			vertical-align:top;
-			margin-top:7px;
+			margin-top:8px;
 			width:22px;
 			height:12px;
 			@include bg-image("img/bulletin");
@@ -187,6 +239,114 @@ export default{
 		background:red;
 		z-index:-1;
 		filter:blur(10px);
+	}
+	.detail{
+		position:fixed;
+		z-index:100;
+		top:0;
+		left:0;
+		width:100%;
+		height:100%;
+		overflow:auto;
+		background:rgba(7,17,27,0.8);
+		backdrop-filter:blur(10px);
+		&.fade-enter-active,&.fade-leave-active{
+			transition: all 0.5s;
+		}
+		&.fade-enter,&.fade-leave-to{
+		  opacity:0;
+		}
+		.detail-wrapper{
+			width:100%;
+			min-height: 100%;
+			.detail-main{
+				margin-top:64px;
+			  padding-bottom:64px;
+			  .name{
+			  	line-height:16px;
+			  	text-align:center;
+			  	font-size:16px;
+			  	font-weight: 700;
+			  }
+			  .star-wrapper{
+			  	 margin-top:16px;
+			  	 padding:2px 0;
+			  	 text-align:center;
+			  }
+			  .title{
+			  	display:flex;
+			  	width:80%;
+			  	margin:28px auto 24px auto;
+			  	.line{
+			  		flex:1;
+			  		position:relative;
+			  		top:-6px;
+			  		border-bottom:1px solid rgba(255,255,255,0.2);
+			  	}
+			  	.text{
+			  		padding:0 12px;
+			  		font-size:15px;
+			  		font-weight:700;
+			  	}
+			  }
+			  .supports{
+			  	width:80%;
+			  	margin:0 auto;
+			  	.support-item{
+			  		padding:0 12px;
+			  		margin-bottom:12px;
+			  		font-size:0px;
+			  		&:last-child{
+			  			margin-bottom:0;
+			  		}
+			  		.icon{
+			  			display:inline-block;
+			  			vertical-align: top;
+			  			width:16px;
+			  			height:16px;
+			  			margin-right:6px;
+			  			background-size:16px 16px;
+			  			background-repeat: no-repeat;
+			  			&.decrease {
+								@include bg-image("img/decrease_2");
+							}
+							&.discount {
+								@include bg-image("img/discount_2");
+							}
+							&.guarantee {
+								@include bg-image("img/guarantee_2");
+							}
+							&.invoice {
+								@include bg-image("img/invoice_2");
+							}
+							&.special {
+								@include bg-image("img/special_2");
+							}
+			  		}
+			  		.text{
+			  			line-height:16px;
+			  			font-size:12px;
+			  		}
+			  	}
+			  }
+			  .bulletin{
+			  	width:80%;
+			  	margin:0 auto;
+			  	.content{
+			  		padding:0 12px;
+			  		line-height:24px;
+			  		font-size:12px;
+			  	}
+			  }
+			}
+		}
+		.detail-close{
+			position:relative;
+			width:32px;
+			height:32px;
+			margin:-64px auto 0 auto;
+			font-size:32px;
+		}
 	}
 }
 </style>
---
+++ src/components/star/star.vue
@@ -0,0 +1,119 @@
+<template>
+	<div class="star" :class="starType">
+		<span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+	</div>
+</template>
+
+<script>
+	
+	const LENGTH=5;
+	const CLS_ON="on";
+	const CLS_HALF="half";
+	const CLS_OFF="off";
+	
+	export default{
+		props:{
+			size:{
+				type:Number
+			},
+			score:{
+				type:Number
+			}
+		},
+		computed:{
+			starType(){
+				return 'star-'+this.size;
+			},
+			itemClasses(){
+				let result=[];
+				let score=Math.floor(this.score * 2)/2;
+				let hasDecimal=score % 1 !==0; //是否有半星
+				let integer=Math.floor(score); //全星个数
+				
+				//全星
+				for(let i=0;i<integer;i++){
+					result.push(CLS_ON);
+				}
+				//半星，一个
+				if(hasDecimal){
+					result.push(CLS_HALF);
+				}
+				//无星
+				while(result.length<LENGTH){
+					result.push(CLS_OFF);
+				}
+				return result;
+			}
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+  @import "../../common/styles/mixin.scss";
+  .star{
+  	.star-item{
+  	   display: inline-block;
+  	   background-repeat: no-repeat;	
+  	}
+  	&.star-48{
+  	   .star-item{
+  	   	  width:20px;
+  	   	  height:20px;
+  	   	  margin-right:22px;
+  	   	  background-size:20px 20px;
+  	   	  &:last-child{
+  	   	  	margin-right:0;
+  	   	  }
+  	   	  &.on{
+  	   	  	@include bg-image('img/star48_on');
+  	   	  }
+  	   	  &.half{
+  	   	  	@include bg-image('img/star48_half');
+  	   	  }
+  	   	  &.off{
+  	   	  	@include bg-image('img/star48_off');
+  	   	  }
+  	   }
+  	}
+  	&.star-36{
+  		.star-item{
+  	   	  width:15px;
+  	   	  height:15px;
+  	   	  margin-right:6px;
+  	   	  background-size:15px 15px;
+  	   	  &:last-child{
+  	   	  	margin-right:0;
+  	   	  }
+  	   	  &.on{
+  	   	  	@include bg-image('img/star36_on');
+  	   	  }
+  	   	  &.half{
+  	   	  	@include bg-image('img/star36_half');
+  	   	  }
+  	   	  &.off{
+  	   	  	@include bg-image('img/star36_off');
+  	   	  }
+  	   }
+  	}
+  	&.star-24{
+  		.star-item{
+  	   	  width:10px;
+  	   	  height:10px;
+  	   	  margin-right:3px;
+  	   	  background-size:10px 10px;
+  	   	  &:last-child{
+  	   	  	margin-right:0;
+  	   	  }
+  	   	  &.on{
+  	   	  	@include bg-image('img/star24_on');
+  	   	  }
+  	   	  &.half{
+  	   	  	@include bg-image('img/star24_half');
+  	   	  }
+  	   	  &.off{
+  	   	  	@include bg-image('img/star24_off');
+  	   	  }
+  	   }
+  	}
+  }
+</style>
