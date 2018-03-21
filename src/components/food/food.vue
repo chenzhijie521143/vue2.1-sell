a8266a1f33854e9770f23991d29766791e29cf3d
chenzhijie <chenzhijie521143@163.com>
2018-03-21 16:27:05 +0800

    food finish

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -35,13 +35,30 @@
 			    <div class="rating">
 			    	<h1 class="title">商品评价</h1>
 			    	<ratingselect
-			    		:select-type="selectType"
+			    		:select-type="this.selectType"
 			    		:only-content="onlyContent"
 			    		:desc="desc"
 			    		:ratings="food.ratings"
 			    		@ratingtype-select="changeSelectType"
 			    		@content-toggle="toggleOnlyContent">
 			        </ratingselect>
+			        <div class="rating-wrapper">
+			        	<ul v-show="food.ratings && food.ratings.length">
+			        		<li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item">
+			        			<div class="user">
+			        				<span class="name">{{rating.username}}</span>
+			        			    <img class="avatar" width="12" height="12" :src="rating.avatar">
+			        			</div>
+			        			<div class="time">{{rating.rateTime | formatDate}}</div>
+			        			<p class="text">
+			        				<span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>
+			        				{{rating.text}}
+			        			</p>
+			        		</li>
+			        	</ul>
+			        	<div class="no-rating"
+			        		v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+			        </div>
 			    </div>
 			</div>
 	    </div>
@@ -49,91 +66,117 @@
 </template>
 
 <script>
-	import Vue from 'vue';
-	import BScroll from 'better-scroll';
-	import cartcontrol from'../cartcontrol/cartcontrol.vue';
-	import split from'../split/split.vue';
-	import ratingselect from '../ratingselect/ratingselect.vue';
-	
-	const POSITIVE = 0;  // 推荐
-	const NEGATIVE = 1;  // 吐槽
-	const ALL = 2;  // 全部
-	
-	export default{
-		props:{
-			food:{
-				type:Object
-			}
-		},
-		data(){
-			return{
-				showFlag:false,
-				selectType:ALL,
-				onlyContent:true,
-				desc:{
-					all:'全部',
-					positive:'推荐',
-					negative:'吐槽'
-				}
-			};
-		},
-		methods:{
-			// 显示
-			show(){
-				// 初始化
-				this.selectType = ALL;
-				this.onlyContent = false;
-				
-				this.showFlag = true;
-				this.$nextTick(() => {
-					if(!this.scroll){
-						this.scroll=new BScroll(this.$refs.food,{
-							click:true
-						});
-					}else{
-						this.scroll.refresh();
-					}
-					
-				});
-			},
-			// 隐藏
-			hide(){
-				this.showFlag=false;
-			},
-			// 加入购物车
-			addFirst(event){
-				if(!event._constructed){
-				    return;
-			    }
-				this.$emit('add-to-cart',event.target);
-				Vue.set(this.food,'count',1);
-			},
-			// 注册事件 实现动画小球
-			addToCart(target){
-				if(!event._constructed){
-				    return;
-			    }
-				this.$emit('add-to-cart',target);
-			},
-			// 改变 selectType
-			changeSelectType(type){
-				this.selectType=type;
-			},
-			// 改变 onlyContent
-			toggleOnlyContent(con){
-				this.onlyContent=con;
-			}
-		},
-		// 注册组件
-		components:{
-			cartcontrol,
-			split,
-			ratingselect
+import Vue from 'vue';
+import BScroll from 'better-scroll';
+import cartcontrol from'../cartcontrol/cartcontrol.vue';
+import split from'../split/split.vue';
+import ratingselect from '../ratingselect/ratingselect.vue';
+import {formatDate} from '../../common/js/date.js';
+
+const POSITIVE = 0;  // 推荐
+const NEGATIVE = 1;  // 吐槽
+const ALL = 2;  // 全部
+
+export default{
+	props:{
+		food:{
+			type:Object
 		}
-	};
+	},
+	data(){
+		return{
+			showFlag:false,
+			selectType:ALL,
+			onlyContent:false,
+			desc:{
+				all:'全部',
+				positive:'推荐',
+				negative:'吐槽'
+			}
+		};
+	},
+	methods:{
+		// 显示
+		show(){
+			// 初始化
+			this.selectType = POSITIVE;
+			this.onlyContent = false;
+			this.showFlag = true;
+			this.$nextTick(() => {
+				if(!this.scroll){
+					this.scroll=new BScroll(this.$refs.food,{
+						click:true
+					});
+				}else{
+					this.scroll.refresh();
+				}
+				
+			});
+		},
+		// 隐藏
+		hide(){
+			this.showFlag=false;
+		},
+		// 加入购物车
+		addFirst(event){
+			if(!event._constructed){
+			    return;
+		    }
+			this.$emit('add-to-cart',event.target);
+			Vue.set(this.food,'count',1);
+		},
+		// 注册事件 实现动画小球
+		addToCart(target){
+			if(!event._constructed){
+			    return;
+		    }
+			this.$emit('add-to-cart',target);
+		},
+		// 改变 selectType
+		changeSelectType(type){
+			this.selectType=type;
+			this.$nextTick(() => {
+				this.scroll.refresh();
+			});
+		},
+		// 改变 onlyContent
+		toggleOnlyContent(con){
+			this.onlyContent=con;
+			this.$nextTick(() => {
+				this.scroll.refresh();
+			});
+		},
+		// 判断 评论是否显示
+		needShow(type,text){
+			// 先判断是否存在内容和是否只显示有内容的评价
+			if(this.onlyContent && !text){
+				return false;
+			}
+			// 判断是否显示全部评价
+			if(this.selectType === ALL){
+				return true;
+			}else{  // 显示选择类型对应的评价
+				return type === this.selectType;
+			}
+		}
+	},
+	filters:{
+		formatDate(time){
+			let date=new Date(time);
+			return formatDate(date,'yyyy-MM-dd hh:mm');
+		}
+	},
+	// 注册组件
+	components:{
+		cartcontrol,
+		split,
+		ratingselect
+	}
+};
 </script>
 
 <style lang="scss" scoped>
+@import "../../common/styles/mixin.scss";
 .food{
 	position:fixed;
 	left:0;
@@ -250,6 +293,58 @@
     		font-size:14px;
     		color:rgb(7,17,27);
     		margin-left:18px;
+    	}
+    	.rating-wrapper{
+    		padding:0 18px;
+    		.rating-item{
+    			position:relative;
+    			padding:16px 0;
+    			@include border-b-1px(rgba(7, 17, 27, 0.1));
+    			.user{
+    				position:absolute;
+    				right:0;
+    				top:16px;
+    				line-height:12px;
+    				font-size:0;
+    				.name{
+    					display:inline-block;
+    					margin-right:6px;
+    					vertical-align:top;
+    					font-size:10px;
+    					color:rgb(147,153,159);
+    				}
+    				.avatar{
+    					border-radius:50%;
+    				}
+    			}
+    			.time{
+    				margin-bottom:6px;
+    				line-height:12px;
+    				font-size:10px;
+    				color:rgb(147,153,159);
+    			}
+    			.text{
+    				line-height:16px;
+    				font-size: 12px;
+    				color:rgb(7,17,27);
+    				.icon-thumb_up,.icon-thumb_down{
+    					margin-right:4px;
+    					line-height: 16px;
+    					font-size:12px;
+    				}
+    				.icon-thumb_up{
+    					color:rgb(0,160,220);
+    				}
+    				.icon-thumb_down{
+    					color:rgb(147,153,159);
+    				}
+    			}
+    		}
+    		.no-rating{
+    			padding:16px 0;
+    			font-size:12px;
+    			color:rgb(147,153,159);
+    		}
     	}
     }
     
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -2,17 +2,17 @@
     <div class="ratingselect">
         <div class="rating-type border-b-1px">
             <span class="block positive"
-            	:class="{'active':sType===2}"
+            	:class="{'active':selectType===2}"
             	@click="select(2,$event)">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
             <span class="block positive"
-            	:class="{'active':sType===0}"
+            	:class="{'active':selectType===0}"
             	@click="select(0,$event)">{{desc.positive}}<span class="count">{{positives.length}}</span></span>
             <span class="block negative"
-            	:class="{'active':sType===1}"
+            	:class="{'active':selectType===1}"
             	@click="select(1,$event)">{{desc.negative}}<span class="count">{{nagetives.length}}</span></span>
         </div>
         <div class="switch" 
-        	:class="{'on':oContent}"
+        	:class="{'on':onlyContent}"
         	@click="toggleContent">
         	<span class="icon-check_circle"></span>
         	<span class="text">只看有内容的评价</span>
@@ -92,62 +92,62 @@
 </script>
 
 <style lang="scss" scoped>
-    @import "../../common/styles/mixin.scss";
-    .ratingselect{
-   	    .rating-type{
-   	        padding:18px 0;
-   	        margin:0 18px;
-   	        @include border-b-1px(rgba(7, 17, 27, 0.1));
-   	        font-size:0;
-   	        .block{
-   	        	display:inline-block;
-   	        	padding:8px 12px;
-   	        	border-radius:1px;
-   	        	font-size:12px;
-   	        	margin-right:8px;
-   	        	color:rgb(77,85,93);
-   	        	&.active{
-   	        		color:#fff;
-   	        	}
-   	        	.count{
-   	        		font-size:8px;
-   	        		margin-left:2px;
-   	        	}
-   	            &.positive{
-   	            	background:rgba(0,160,220,0.2);
-   	            	&.active{
-   	            		background:rgb(0,160,220);
-   	            	}
-   	            }
-   	            &.negative{
-   	            	background:rgba(77,85,93,0.2);
-   	            	&.active{
-   	            		background:rgb(77,85,93);
-   	            	}
-   	            }
-   	        }	
-   	    }
-   	    .switch{
-   	    	padding:12px 18px;
-   	    	line-height: 24px;
-   	    	border-bottom:1px solid rgba(7,17,27,0.1);
-   	    	color:rgb(147,153,159);
-   	    	font-size:0;
-   	    	&.on{
-   	    		.icon-check_circle{
-   	    			color:#00c850;
-   	    		}
-   	    	}
-   	    	.icon-check_circle{
-   	    		display:inline-block;
-   	    		vertical-align:top;
-   	    		margin-right:4px;
-   	    		font-size:24px;
-   	    	}
-   	    	.text{
-   	    		display:inline-block;
-   	    		font-size:12px;
-   	    	}
-   	    }
+@import "../../common/styles/mixin.scss";
+.ratingselect{
+    .rating-type{
+        padding:18px 0;
+        margin:0 18px;
+        @include border-b-1px(rgba(7, 17, 27, 0.1));
+        font-size:0;
+        .block{
+        	display:inline-block;
+        	padding:8px 12px;
+        	border-radius:1px;
+        	font-size:12px;
+        	margin-right:8px;
+        	color:rgb(77,85,93);
+        	&.active{
+        		color:#fff;
+        	}
+        	.count{
+        		font-size:8px;
+        		margin-left:2px;
+        	}
+            &.positive{
+            	background:rgba(0,160,220,0.2);
+            	&.active{
+            		background:rgb(0,160,220);
+            	}
+            }
+            &.negative{
+            	background:rgba(77,85,93,0.2);
+            	&.active{
+            		background:rgb(77,85,93);
+            	}
+            }
+        }	
     }
+    .switch{
+    	padding:12px 18px;
+    	line-height: 24px;
+    	border-bottom:1px solid rgba(7,17,27,0.1);
+    	color:rgb(147,153,159);
+    	font-size:0;
+    	&.on{
+    		.icon-check_circle{
+    			color:#00c850;
+    		}
+    	}
+    	.icon-check_circle{
+    		display:inline-block;
+    		vertical-align:top;
+    		margin-right:4px;
+    		font-size:24px;
+    	}
+    	.text{
+    		display:inline-block;
+    		font-size:12px;
+    	}
+    }
+}
 </style>
