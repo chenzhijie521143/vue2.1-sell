2d6e3ac67a8d085271a999d936220b546f9b3d6d
chenzhijie <chenzhijie521143@163.com>
2018-03-23 12:10:51 +0800

    build test

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -26,7 +26,6 @@ var ratings=appData.ratings;
 //创建一个路由
 var apiRoutes=express.Router();
 
-app.use('/api',apiRoutes);
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -38,24 +37,25 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   // these devServer options should be customized in /config/index.js
   devServer: {
   	before(app) {
-      app.get('/api/seller', function(req, res) {
+      apiRoutes.get('/seller', function(req, res) {
         res.json({
           errno: 0,
           data: seller
         })
       });
-      app.get('/api/goods', function(req, res) {
+      apiRoutes.get('/goods', function(req, res) {
         res.json({
           errno: 0,
           data: goods
         })
       });
-      app.get('/api/ratings', function(req, res) {
+      apiRoutes.get('/ratings', function(req, res) {
         res.json({
           errno: 0,
           data: ratings
         })
       });
+      app.use('/api',apiRoutes);
     },
     clientLogLevel: 'warning',
     historyApiFallback: {
